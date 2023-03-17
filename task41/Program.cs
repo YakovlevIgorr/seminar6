b46c14108c0927b19cf916e5d3b353c2d661bb15
Igor <jaknikig@mail.ru>
2023-03-18 01:00:46 +0600

    init2

---
+++ task41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double b1 = 0;
+double k1 = 0;
+double b2 = 0;
+double k2 = 0;
+double x = 0;
+double y = 0;
+
+
+double inputData(string point)
+{
+    Console.WriteLine($"Введите значение {point} ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+double found_X(double bb2, double bb1, double kk1, double kk2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double found_Y(double kk1, double x, double bb1)
+{
+    double y = (k1 * x) + b1;
+    return y;
+}
+
+void printCoord()
+{
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k1 = {k1} -> ({x};{y})");
+}
+
+b1 = inputData("b1");
+k1 = inputData("k1");
+b2 = inputData("b2");
+k2 = inputData("k2");
+x = found_X(b2,b1,k1,k2);
+y = found_Y(k1,x,b1);
+printCoord();
+
+
+
+
+
+
+
---
+++ task43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+Console.WriteLine("Введите количество чисел ");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] arry = new int[number];
+
+void fillArry()// 
+{
+    for(int i = 0;i < arry.Length; i++)
+{
+    Console.WriteLine("Введите число " + (i + 1) + " ");
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    arry[i] = numbers;
+}
+}
+
+int countPositive(int [] array)
+{
+    int count = 0;
+    for(int k = 0; k < array.Length; k++)
+    {
+        if(array[k] > 0) count ++;
+    }
+    return count;
+}
+
+void printArray(int [] totalArray)
+{
+    System.Console.WriteLine($"[{string.Join(",",arry)}] --> {countPositive(arry)}");
+}
+
+fillArry();
+printArray(arry);
